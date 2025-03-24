a5576f4080319ba9616740def70d273a7dfdde61
Girish1webomindapps <girish@webomindapps.com>
2025-03-24 19:05:57 +0530

    24-03-2025   Bank verification added
    24-03-2025   Bank verification added

--- resources/views/admin/adms/compliance/pending-bank_account/index.blade.php
+++ resources/views/admin/adms/compliance/pending-bank_account/index.blade.php
@@ -1,86 +1,78 @@
 <x-applayout>
-    <x-applayout>
-        <x-admin.breadcrumb title="Pending Bank Approvals">    
-        </x-admin.breadcrumb>
-        <div class="row">
-            <div class="col-lg-12">
-                @php
-                    $columns = [
-                        ['label' => 'Id', 'column' => 'id', 'sort' => true],
-                        ['label' => 'Client Name', 'column' => 'entity_name', 'sort' => true],
-                        ['label' => 'Employee Name', 'column' => 'emp_name', 'sort' => true],
-                        ['label' => 'Phone', 'column' => 'phone1', 'sort' => true],
-                        ['label' => 'Actions', 'column' => 'action', 'sort' => false],
-                    ];
-                @endphp
-                <x-table :columns="$columns" :data="$candidate" :checkAll=false :bulk="route('admin.cfis.bulk')" :route="route('admin.candidatemaster')">
-    
-                    <x-slot:filters>
-    
-                        <form action="{{ route('admin.candidatemaster.export') }}" method="POST">
-                            @csrf
-                            <div class="row">
-                                <div class="col-lg-3">
-                                    <input type="date" class="form-control" name="from_date" id="from_date">
-                                </div>
-                                <div class="col-lg-1 text-center my-auto">
-                                    <span class="fw-semibold fs-6">To</span>
-                                </div>
-                                <div class="col-lg-3">
-                                    <input type="date" class="form-control" name="to_date" id="to_date">
-                                </div>
-                                <div class="col-3">
-                                    <button type="submit" class="add-btn bg-success text-white">Export</button>
-                                </div>
+    <x-admin.breadcrumb title="Pending Bank Approvals">
+    </x-admin.breadcrumb>
+    <div class="row">
+        <div class="col-lg-12">
+            @php
+                $columns = [
+                    ['label' => 'Id', 'column' => 'id', 'sort' => true],
+                    ['label' => 'Bank Name', 'column' => 'bank_name', 'sort' => true],
+                    ['label' => 'Bank Account Number', 'column' => 'bank_account_no', 'sort' => true],
+                    ['label' => 'Bank IFSC Code', 'column' => 'bank_ifsc_code', 'sort' => true],
+                    ['label' => 'Approval Status', 'column' => 'bank_status', 'sort' => true],
+                    ['label' => 'Actions', 'column' => 'action', 'sort' => false],
+                ];
+            @endphp
+            <x-table :columns="$columns" :data="$pendingbank" :checkAll=false :bulk="route('admin.cfis.bulk')" :route="route('admin.pendingbankapprovals')">
+
+                <x-slot:filters>
+
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-lg-3">
+                                <input type="date" class="form-control" name="from_date" id="from_date">
                             </div>
-                        </form>
-    
-                    </x-slot:filters>
-                    @foreach ($candidate as $key => $item)
-                        <tr>
-    
-                            <td>{{ $item->id }}</td>
-                            <td>
-                                {{ $item->entity_name }}
-                            </td>
-                            <td> {{ $item->emp_name }}</td>
-                            <td> {{ $item->phone1 }}</td>
-    
-                            <td>
-                                <div class="dropdown pop_Up dropdown_bg">
-                                    <div class="dropdown-toggle" id="dropdownMenuButton-{{ $item->id }}"
-                                        data-bs-toggle="dropdown" aria-expanded="true">
-                                        Action
-                                    </div>
-                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1"
-                                        style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate(-95px, -25.4219px);"
-                                        data-popper-placement="top-end">
-                                        <li>
-                                            <a class="dropdown-item"
-                                                href="{{ route('admin.candidatemaster.edit', $item) }}">
-                                                <i class='bx bx-edit-alt'></i>
-                                                Edit
-                                            </a>
-                                           
-                                            <a href="{{ route('admin.candidatemaster.view', $item->id) }}"
-                                                class="dropdown-item">
-                                                <i class='bx bx-link-alt'></i> View
-                                            </a>
-                                            <a class="dropdown-item"
-                                                href="{{ route('admin.candidatemaster.download', $item->id) }}">
-                                                <i class='bx bxs-download'></i>
-                                                Download pdf
-                                            </a>
-                                        </li>
-                                    </ul>
+                            <div class="col-lg-1 text-center my-auto">
+                                <span class="fw-semibold fs-6">To</span>
+                            </div>
+                            <div class="col-lg-3">
+                                <input type="date" class="form-control" name="to_date" id="to_date">
+                            </div>
+                        </div>
+                    </form>
+
+                </x-slot:filters>
+                @foreach ($pendingbank as $key => $item)
+                    <tr>
+
+                        <td>{{ $item->id }}</td>
+                        <td>
+                            {{ $item->bank_name }}
+                        </td>
+                        <td> {{ $item->bank_account_no }}</td>
+                        <td> {{ $item->bank_ifsc_code }}</td>
+                        <td>
+                            @if ($item->bank_status == 0)
+                                <span class="badge rounded-pill deactive">Pending</span>
+                            @else
+                                <span class="badge rounded-pill sactive">Complected</span>
+                            @endif
+                        </td>
+
+                        <td>
+                            <div class="dropdown pop_Up dropdown_bg">
+                                <div class="dropdown-toggle" id="dropdownMenuButton-{{ $item->id }}"
+                                    data-bs-toggle="dropdown" aria-expanded="true">
+                                    Action
                                 </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </x-table>
-            </div>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1"
+                                    style="position: absolute; inset: auto auto 0px 0px; margin: 0px; transform: translate(-95px, -25.4219px);"
+                                    data-popper-placement="top-end">
+                                    <li>
+                                        <a class="dropdown-item"
+                                            href="{{ route('admin.pendingbankapprovals.edit', $item->id) }}">
+                                            <i class='bx bx-edit-alt'></i>
+                                            Edit
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </x-table>
         </div>
-       
-    </x-applayout>
-    
+    </div>
+
 </x-applayout>
